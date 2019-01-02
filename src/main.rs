04ffc78d7463ec950a53da29e662a60d5f3a0bf9
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2019-01-03 02:50:23 +0300

    WIP - Loading from git

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,27 @@ extern crate gtk;
 use gtk::prelude::*;
 
 fn main() -> Result<(), String> {
+    let args = std::env::args_os().collect::<Vec<_>>();
+
+    let repo_path = args.get(1).ok_or("Not passed path to git repository")?;
+
+    println!("Loading git repository {}", repo_path.to_string_lossy());
+
     gtk::init().map_err(|_| "Failed to initialize Gtk+".to_string())?;
 
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
-    window.set_title("Привет, мир!");
+    window.set_title(&repo_path.to_string_lossy());
 
     let list_store = gtk::ListStore::new(&[gtk::Type::String]);
 
     let tree_view = gtk::TreeView::new();
+
+    let scrolled_window = gtk::ScrolledWindow::new(
+        None,
+        &tree_view.get_vadjustment()
+    );
+
+    scrolled_window.add(&tree_view);
 
     tree_view.set_model(&list_store);
 
@@ -22,13 +35,35 @@ fn main() -> Result<(), String> {
 
     tree_view.append_column(&column);
 
-    list_store.insert_with_values(
-        None,
-        &[0],
-        &[&"Initial commit"]
-    );
+    {
+        use std::process::Command;
 
-    window.add(&tree_view);
+        let mut cmd = Command::new("git");
+        cmd
+            .current_dir(&repo_path)
+            .args(&["log", "--oneline"]);
+
+        println!("Running {:?} at {:?}", cmd, repo_path);
+
+        let cmd_output = cmd
+            .output()
+            .map_err(|e| e.to_string())?;
+
+        if !cmd_output.status.success() {
+            return Err(format!("Error executing {:?}:\n{}", cmd, String::from_utf8_lossy(&cmd_output.stderr)));
+        }
+
+        let output_str = String::from_utf8_lossy(&cmd_output.stdout);
+        for line in output_str.lines() {
+            list_store.insert_with_values(
+                None,
+                &[0],
+                &[&line]
+            );
+        }
+    }
+
+    window.add(&scrolled_window);
 
     window.show_all();
 
