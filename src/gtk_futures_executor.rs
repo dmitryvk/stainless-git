255d21d722aa661b80aeb81d7406397e9085bcbf
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2019-01-03 21:47:50 +0300

    WIP - Use futures with futures-cpupool to asynchronously call git

---
+++ src/gtk_futures_executor.rs
@@ -0,0 +1,81 @@
+type BoxedFuture = Box<futures::future::Future<Item=(), Error=()>>;
+
+struct GtkEventLoopAsyncExecutorBackend {
+    main_thread_id: std::thread::ThreadId,
+    next_id: std::sync::atomic::AtomicUsize,
+    spawns: std::cell::RefCell<std::collections::HashMap<usize, futures::executor::Spawn<BoxedFuture>>>,
+}
+
+#[derive(Clone)]
+pub struct GtkEventLoopAsyncExecutor {
+    backend: std::sync::Arc<GtkEventLoopAsyncExecutorBackend>,
+}
+
+// BoxedFuture is only invoked from glib::source::idle_add callback which is sequentially called from single thread
+unsafe impl Send for GtkEventLoopAsyncExecutor{}
+unsafe impl Sync for GtkEventLoopAsyncExecutor{}
+
+impl GtkEventLoopAsyncExecutor {
+    pub fn new() -> Self {
+        GtkEventLoopAsyncExecutor {
+            backend: std::sync::Arc::new(
+                GtkEventLoopAsyncExecutorBackend {
+                    main_thread_id: std::thread::current().id(),
+                    next_id: std::sync::atomic::AtomicUsize::new(0),
+                    spawns: std::cell::RefCell::new(std::collections::HashMap::new())
+                }
+            )
+        }
+    }
+
+    pub fn spawn<F: futures::future::Future<Item=(), Error=()> + Sized + 'static>(&self, f: F) {
+        let id = self.backend.next_id.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        {
+            let mut spawns = self.backend.spawns.borrow_mut();
+            let spawn = futures::executor::spawn(Box::new(f) as Box<futures::future::Future<Item=(), Error=()>>);
+            spawns.insert(id, spawn);
+        }
+
+        use futures::executor::Notify;
+        self.notify(id);
+    }
+
+    fn invoke(&self, id: usize) {
+        let mut spawns = self.backend.spawns.borrow_mut();
+        match spawns.remove(&id) {
+            None => {
+                eprintln!("Attempted to invoke non-existing spawn {}", id);
+            },
+            Some(mut spawn) => {
+                let result = spawn.poll_future_notify(
+                    &futures::executor::NotifyHandle::from(
+                        std::sync::Arc::new(self.clone())
+                    ),
+                    id
+                );
+                
+                match result {
+                    Ok(futures::Async::Ready(_)) => {
+                        // Do nothing
+                    },
+                    Ok(futures::Async::NotReady) => {
+                        spawns.insert(id, spawn);
+                    },
+                    Err(_) => {
+                        eprintln!("Spawned future {} returned error", id);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl futures::executor::Notify for GtkEventLoopAsyncExecutor {
+    fn notify(&self, id: usize) {
+        let executor = self.clone();
+        glib::source::idle_add(move || {
+            executor.invoke(id);
+            glib::source::Continue(false)
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+extern crate glib;
 extern crate gtk;
+extern crate futures;
+extern crate futures_cpupool;
+
+mod gtk_futures_executor;
 
 use gtk::prelude::*;
+use futures::prelude::*;
+use futures_cpupool::CpuPool;
+
+use crate::gtk_futures_executor::GtkEventLoopAsyncExecutor;
 
 fn main() -> Result<(), String> {
     let args = std::env::args_os().collect::<Vec<_>>();
 
-    let repo_path = args.get(1).ok_or("Not passed path to git repository")?;
+    let repo_path = args.get(1).ok_or("Not passed path to git repository")?.clone();
 
     println!("Loading git repository {}", repo_path.to_string_lossy());
 
     gtk::init().map_err(|_| "Failed to initialize Gtk+".to_string())?;
+
+    let gtk_executor = GtkEventLoopAsyncExecutor::new();
+    let cpupool = CpuPool::new_num_cpus();
 
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
     window.set_title(&repo_path.to_string_lossy());
@@ -18,10 +30,7 @@ fn main() -> Result<(), String> {
 
     let tree_view = gtk::TreeView::new();
 
-    let scrolled_window = gtk::ScrolledWindow::new(
-        None,
-        &tree_view.get_vadjustment()
-    );
+    let scrolled_window = gtk::ScrolledWindow::new(None, None);
 
     scrolled_window.add(&tree_view);
 
@@ -35,35 +44,10 @@ fn main() -> Result<(), String> {
 
     tree_view.append_column(&column);
 
-    {
-        use std::process::Command;
-
-        let mut cmd = Command::new("git");
-        cmd
-            .current_dir(&repo_path)
-            .args(&["log", "--oneline"]);
-
-        println!("Running {:?} at {:?}", cmd, repo_path);
-
-        let cmd_output = cmd
-            .output()
-            .map_err(|e| e.to_string())?;
-
-        if !cmd_output.status.success() {
-            return Err(format!("Error executing {:?}:\n{}", cmd, String::from_utf8_lossy(&cmd_output.stderr)));
-        }
-
-        let output_str = String::from_utf8_lossy(&cmd_output.stdout);
-        for line in output_str.lines() {
-            list_store.insert_with_values(
-                None,
-                &[0],
-                &[&line]
-            );
-        }
-    }
-
     window.add(&scrolled_window);
+
+    window.set_default_size(300, 300);
+    window.set_position(gtk::WindowPosition::Center);
 
     window.show_all();
 
@@ -71,6 +55,94 @@ fn main() -> Result<(), String> {
         gtk::main_quit();
         Inhibit(false)
     });
+
+    {
+        list_store.insert_with_values(
+            None,
+            &[0],
+            &[&"Loading..."]
+        );
+    }
+
+    let list_store_1 = list_store.clone();
+    let list_store_2 = list_store.clone();
+    let cpupool_1 = cpupool.clone();
+    let cpupool_2 = cpupool.clone();
+    let window_1 = window.clone();
+    gtk_executor.spawn(futures::future::lazy(move || {
+        cpupool_1.spawn_fn(move || {
+            use std::process::Command;
+
+            std::thread::sleep_ms(1000);
+
+            let mut cmd = Command::new("git");
+            cmd
+                .current_dir(&repo_path)
+                .args(&["log", "--oneline"]);
+
+            let msg = format!("Running {:?} at {:?}", cmd, repo_path);
+
+            futures::future::ok::<_, ()>((cmd, msg))
+        }).and_then(move |(cmd, msg)| {
+            let list_store = list_store_1;
+            list_store.clear();
+            list_store.insert_with_values(
+                None,
+                &[0],
+                &[&msg]
+            );
+
+            cpupool_2.spawn_fn(move || {
+                let mut cmd = cmd;
+                let cmd_output = cmd
+                    .output()
+                    .map_err(|e| e.to_string());
+
+                let cmd_output = match cmd_output {
+                    Ok(ref x) if !x.status.success() => Err(String::from_utf8_lossy(&x.stderr).into_owned()),
+                    _ => cmd_output
+                };
+
+                futures::future::ok::<_, ()>((cmd, cmd_output))
+            })
+        }).and_then(move |(cmd, cmd_output)| {
+            match cmd_output {
+                Ok(cmd_output) => {
+                    let output_str = String::from_utf8_lossy(&cmd_output.stdout).into_owned();
+                    let list_store = list_store_2;
+                    list_store.clear();
+                    for line in output_str.lines() {
+                        list_store.insert_with_values(
+                            None,
+                            &[0],
+                            &[&line]
+                        );
+                    }
+                },
+                Err(msg) => {
+                    use gtk::{ButtonsType, DialogFlags, MessageType, MessageDialog};
+                    let dialog = MessageDialog::new(Some(&window_1),
+                        DialogFlags::empty() | DialogFlags::MODAL,
+                        MessageType::Error,
+                        ButtonsType::Ok,
+                        &msg
+                    );
+                    dialog.run();
+                    dialog.destroy();
+                    let list_store = list_store_2;
+                    list_store.clear();
+                    list_store.insert_with_values(
+                        None,
+                        &[0],
+                        &[&format!("Error executing {:?}:\n{}", cmd, msg)]
+                    );
+                }
+            }
+
+            futures::future::ok::<_, ()>(())
+        })
+    }));
+
 
     gtk::main();
 
